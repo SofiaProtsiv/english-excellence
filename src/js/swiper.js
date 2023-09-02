1c74edb09fc72c6f39034d069ac8fbcbd3214c2a
Mykhalytskyi Vladyslav <vmykhali666@gmail.com>
2023-09-02 23:03:42 +0300

    add swiper api and change styles
    using "!important" for rewrite api class styles

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,13 +1,22 @@
-// import Swiper bundle with all modules installed
-import Swiper from 'swiper/bundle';
-
-// import styles bundle
+import Swiper from 'swiper';
 import 'swiper/css';
 
+const breakpoints = {
+  // when window width is >= 768px
+  320: {
+    slidesPerView: 1,
+  },
+  // when window width is >= 768px
+  768: {
+    slidesPerView: 2,
+  },
+  // when window width is >= 1280px
+  1280: {
+    slidesPerView: 3,
+  },
+};
+
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    speed: 400,
-    spaceBetween: 100,
+  spaceBetween: 32,
+  breakpoints
 });
